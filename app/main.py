5c77ef594d419c9a886f6313933e037793e1e516
Goon981 <dorianlaroche200@gmail.com>
2026-08-06 21:04:22 +0100

    Remove setup router - superadmin already created in database

--- app/main.py
+++ app/main.py
@@ -28,7 +28,6 @@ from .routers import (
     merchant,
     orders,
     payments,
-    setup,
     shops,
     stats,
     storefront,
@@ -91,7 +90,6 @@ app.include_router(stats.router)
 app.include_router(admin.router)
 app.include_router(uploads.router)
 app.include_router(invitations.router)
-app.include_router(setup.router)
 
 # --- Applications HTML ----------------------------------------------------- #
 app.include_router(storefront.router)
